4308e3741ab626aa108a6e1fb0ea164ddc4e8739
Crisbrix <cmateoramirez@ucundinamarca.edu.co>
2026-01-14 14:52:27 -0500

    Fix CORS issue in auth-login endpoint

--- api/auth-login.js
+++ api/auth-login.js
@@ -7,6 +7,14 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 
+// Middleware CORS para permitir el frontend
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  next();
+});
+
 // Endpoint de login
 app.post('/login', async (req, res) => {
   try {
